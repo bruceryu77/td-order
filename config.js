91474fd2ebe2fab05ae94badd66e9870db74b426
bruceryu77 <bruceryu77@users.noreply.github.com>
2026-08-04 12:59:19 -0400

    Revert Kakao share to text order list without web feed button.

--- config.js
+++ config.js
@@ -3,8 +3,10 @@ window.APP_CONFIG = {
   /**
    * Kakao Developers > 내 애플리케이션 > 앱 키 > JavaScript 키
    * Platform: Web
-   * Site domain 예: bruceryu77.github.io
+   * Site domain 예: https://bruceryu77.github.io
    * https://developers.kakao.com/
    */
-  KAKAO_JS_KEY: "244a58f9ded88f4baec18a0a7e0f40ae"
+  KAKAO_JS_KEY: "244a58f9ded88f4baec18a0a7e0f40ae",
+  /** Kakao "자세히 보기" / 버튼이 열 웹 주문 페이지 */
+  ORDER_SITE_URL: "https://bruceryu77.github.io/td-order/"
 };
